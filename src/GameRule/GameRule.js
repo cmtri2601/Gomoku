783404bbb47a2acade25318bfc0b641f1028475e
Cao Minh Trí <minh-tri@system-exe.com.vn>
2022-10-05 17:56:29 +0700

    logic diagon

--- src/GameRule/GameRule.js
+++ src/GameRule/GameRule.js
@@ -76,6 +76,56 @@ const checkVertical = (currentGamer, board, x, y, height) => {
 }
 
 const checkDiagonal = (currentGamer, board, x, y, width, height) => {
+  let continuity = 0;
+  let anchor = 0;
+
+  let top =  x < 5 ? 0 : x - 4;
+  let bottom =  x > height - 5 ? height - 1 : x + 4;
+  let left =  y < 5 ? 0 : y - 4;
+  let right =  y > width - 5 ? width - 1 : y + 4;
+  
+  /** check line: top left to bottom right */
+  let topleft = x - top < y - left ? x - top : y- left;
+  let bottomright = bottom - x < right - y ? bottom - x : right - y;
+  anchor = -topleft;
+  for (let i = -topleft; i <= bottomright; i++) {
+    if (board[x + i][y + i] === currentGamer) 
+      continuity++;
+    else {
+      continuity = 0;
+      anchor = i + 1;
+    }
+
+    if (continuity === 5) {
+      return [{x: x + anchor, y: y + anchor}, 
+        {x: x + anchor + 1, y: y + anchor + 1},
+        {x: x + anchor + 2, y: y + anchor + 2},
+        {x: x + anchor + 3, y: y + anchor + 3},
+        {x: x + anchor + 4, y: y + anchor + 4}
+      ];
+    }
+  }
+  /** check line: top right to bottom left */
+  let topright = x - top < right - y ? x - top : right - y;
+  let bottomleft = bottom - x < y - left ? bottom - x : y - left;
+  anchor = -topright;
+  for (let i = -topright; i <= bottomleft; i++) {
+    if (board[x + i][y - i] === currentGamer) 
+      continuity++;
+    else {
+      continuity = 0;
+      anchor = i + 1;
+    }
+
+    if (continuity === 5) {
+      return [{x: x + anchor, y: y - anchor}, 
+        {x: x + anchor + 1, y: y - anchor + 1},
+        {x: x + anchor + 2, y: y - anchor + 2},
+        {x: x + anchor + 3, y: y - anchor + 3},
+        {x: x + anchor + 4, y: y - anchor + 4}
+      ];
+    }
+  }
   return [];
 }
 
